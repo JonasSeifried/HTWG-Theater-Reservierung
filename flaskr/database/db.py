c2963c9e3942d5f022b2857e1d3ae3b9a09cd245
hannesbrugger <hannes.brugger.007@gmail.com>
2022-11-26 16:53:55 +0100

    added certifi

--- flaskr/database/db.py
+++ flaskr/database/db.py
@@ -1,11 +1,10 @@
-import ssl
-
+import certifi
 from dotenv import load_dotenv, find_dotenv
 from pymongo import MongoClient
 
 
 def get_database():
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient("mongodb+srv://root:roottoor@htwg-theater-reservieru.bzwrqzs.mongodb.net/?retryWrites=true&w=majority",ssl_cert_reqs=ssl.CERT_NONE)
+    client = MongoClient("mongodb+srv://root:roottoor@htwg-theater-reservieru.bzwrqzs.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
 
     return client['theater']
