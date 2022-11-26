178f7de806b09b015ca15d7a69dd22c4fec07cdb
hannesbrugger <hannes.brugger.007@gmail.com>
2022-11-26 16:46:11 +0100

    added mail.py

--- flaskr/database/db.py
+++ flaskr/database/db.py
@@ -1,9 +1,11 @@
+import ssl
+
 from dotenv import load_dotenv, find_dotenv
 from pymongo import MongoClient
 
 
 def get_database():
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient("mongodb+srv://root:roottoor@htwg-theater-reservieru.bzwrqzs.mongodb.net/?retryWrites=true&w=majority")
+    client = MongoClient("mongodb+srv://root:roottoor@htwg-theater-reservieru.bzwrqzs.mongodb.net/?retryWrites=true&w=majority",ssl_cert_reqs=ssl.CERT_NONE)
 
     return client['theater']
